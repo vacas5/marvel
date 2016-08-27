8f1a83efa82566a4894abea07abcab99fe54e716
vacas5 <russell@tandum.com>
2016-08-26 19:43:08 -0500

    Initial layout and bitters setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CharacterList from './CharacterList';
 
@@ -7,14 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App-wrapper">
+          <div className="App-header">
+            <h2 className="App-logo">
+              <img src="http://vignette2.wikia.nocookie.net/spiderman/images/5/53/Marvel-logo-copy1.png" alt="logo" />
+              ATOR
+            </h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <CharacterList></CharacterList>
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CharacterList></CharacterList>
+        <div className="App-footer">
+          <p>Data provided by Marvel. &copy; 2016 Marvel</p>
+        </div>
       </div>
     );
   }
