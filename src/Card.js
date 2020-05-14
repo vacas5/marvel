ba058e12397b6a206de6ffa8da6690579dca5df2
vacas5 <russelljanderson@gmail.com>
2020-05-13 19:03:46 -0500

    prettify some files

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Card.scss';
-import classNames from 'classnames';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Card.scss";
+import classNames from "classnames";
 
 Card.propTypes = {
   id: PropTypes.number,
@@ -10,42 +10,47 @@ Card.propTypes = {
   thumbnail: PropTypes.object,
   urls: PropTypes.array,
   type: PropTypes.string,
-}
+};
 
-export default function Card({ id, name, title, thumbnail = {}, urls = [], type }) {
-    const headers = {
-      "characters": name,
-      "series": title
-    };
-    const cardImageClass = classNames('card-image', {
-      'bottom': thumbnail.path && thumbnail.path.includes('image_not_available')
-    });
-    const cardImageStyle = {
-      backgroundImage: `url(${thumbnail.path}.${thumbnail.extension})`
-    };
+export default function Card({
+  id,
+  name,
+  title,
+  thumbnail = {},
+  urls = [],
+  type,
+}) {
+  const headers = {
+    characters: name,
+    series: title,
+  };
+  const cardImageClass = classNames("card-image", {
+    bottom: thumbnail.path && thumbnail.path.includes("image_not_available"),
+  });
+  const cardImageStyle = {
+    backgroundImage: `url(${thumbnail.path}.${thumbnail.extension})`,
+  };
 
-    return (
-      <div className="card">
-        <div className={cardImageClass} style={cardImageStyle}></div>
-        <div className="card-header">
-          {headers[type]}
-        </div>
-        <div className="card-copy">
-          <ul>
-            {urls.map( (url, index) => {
-              var names = {
-                'detail': 'Detail',
-                'wiki': 'Wiki',
-                'comiclink': 'Comic Link'
-              };
-              return (
-                <li key={`${id}_${index}`}>
-                  <a href={url.url}>{names[url.type]}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+  return (
+    <div className="card">
+      <div className={cardImageClass} style={cardImageStyle}></div>
+      <div className="card-header">{headers[type]}</div>
+      <div className="card-copy">
+        <ul>
+          {urls.map((url, index) => {
+            var names = {
+              detail: "Detail",
+              wiki: "Wiki",
+              comiclink: "Comic Link",
+            };
+            return (
+              <li key={`${id}_${index}`}>
+                <a href={url.url}>{names[url.type]}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    )
+    </div>
+  );
 }
--- src/ContentList.js
+++ src/ContentList.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import AlphaLinks from './AlphaLinks';
-import Loader from './Loader';
-import Card from './Card';
-import Pagination from './Pagination';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import AlphaLinks from "./AlphaLinks";
+import Loader from "./Loader";
+import Card from "./Card";
+import Pagination from "./Pagination";
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 export default class ContentList extends Component {
   constructor(props) {
@@ -13,19 +15,19 @@ export default class ContentList extends Component {
       total: 0,
       page: 0,
       count: 0,
-      offset: 0
+      offset: 0,
     };
   }
 
   static propTypes = {
-      params: PropTypes.object,
-      paging: PropTypes.number,
-  }
+    params: PropTypes.object,
+    paging: PropTypes.number,
+  };
 
   static defaultProps = {
-      params: {},
-      paging: 50
-  }
+    params: {},
+    paging: 50,
+  };
 
   componentDidMount() {
     const { params } = this.props;
@@ -33,13 +35,16 @@ export default class ContentList extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.params.listType !== nextProps.params.listType || this.props.params.letter !== nextProps.params.letter) {
+    if (
+      this.props.params.listType !== nextProps.params.listType ||
+      this.props.params.letter !== nextProps.params.letter
+    ) {
       // visually it looks better to reset this stuff before the fetch
       this.setState({
         list: [],
         total: 0,
         page: 0,
-        offset: 0
+        offset: 0,
       });
       // putting fetch here because we only want to fire on new prop
       this.fetchHandler(nextProps.params.listType, nextProps.params.letter, 0);
@@ -50,86 +55,88 @@ export default class ContentList extends Component {
     // this should eventually be rolled into a utility function
     // pattern to create success / error callbacks (then/catch)
     if (response.ok) {
-        return response.json();
+      return response.json();
     } else {
-        var error = new Error(response.statusText);
-        error.response = response;
-        throw error;
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
     }
-  }
+  };
 
   fetchHandler = (listType, letter, offset) => {
     const { paging } = this.props;
     const key = process.env.REACT_APP_MARVEL_KEY;
     const urls = {
-      'characters': `http://gateway.marvel.com/v1/public/characters?apikey=${key}&orderBy=name&limit=${paging}&offset=${offset}&nameStartsWith=${letter}`,
-      'series': `http://gateway.marvel.com/v1/public/series?apikey=${key}&orderBy=title&limit=${paging}&offset=${offset}&titleStartsWith=${letter}`
+      characters: `http://gateway.marvel.com/v1/public/characters?apikey=${key}&orderBy=name&limit=${paging}&offset=${offset}&nameStartsWith=${letter}`,
+      series: `http://gateway.marvel.com/v1/public/series?apikey=${key}&orderBy=title&limit=${paging}&offset=${offset}&titleStartsWith=${letter}`,
     };
 
-    return fetch(urls[listType]).then(this.errorHandler).then(json => {
-      this.setState({
-        list: json.data.results,
-        total: json.data.total,
-        count: json.data.count
+    return fetch(urls[listType])
+      .then(this.errorHandler)
+      .then((json) => {
+        this.setState({
+          list: json.data.results,
+          total: json.data.total,
+          count: json.data.count,
+        });
       });
-    });
-  }
+  };
 
   resetPage = (page) => {
     const { params, paging } = this.props;
-    const offset = (page * paging) || 0;
+    const offset = page * paging || 0;
     // visually it looks better to reset this stuff before the fetch
     this.setState({
       list: [],
       total: 0,
       page: page,
-      offset: offset
+      offset: offset,
     });
     this.fetchHandler(params.listType, params.letter, offset);
-  }
+  };
 
   handleDecrement = () => {
-    this.resetPage(this.state.page - 1)
-  }
+    this.resetPage(this.state.page - 1);
+  };
 
   handleIncrement = () => {
-    this.resetPage(this.state.page + 1)
-  }
+    this.resetPage(this.state.page + 1);
+  };
 
   render() {
     const { params } = this.props;
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const { list } = this.state;
 
     return (
       <div>
         <h2 className="main_title">{params.listType}</h2>
         <ul className="nav_items">
-          {alphabet.map((letter, index) => <AlphaLinks
+          {alphabet.map((letter, index) => (
+            <AlphaLinks
               letter={letter}
               route={`/${params.listType}/${letter}`}
               key={index}
               selected={params.letter === letter}
-          />)}
+            />
+          ))}
         </ul>
         <div className="cards">
-          {this.state.list.map( (ser) => {
-            return (
-              <Card key={ser.id} {...ser} type={params.listType} />
-            );
+          {list.map((ser) => {
+            return <Card key={ser.id} {...ser} type={params.listType} />;
           })}
         </div>
-        {this.state.list.length ?
-            <Pagination
-                page={this.state.page}
-                count={this.state.count}
-                offset={this.state.offset}
-                total={this.state.total}
-                onDecrement={this.handleDecrement}
-                onIncrement={this.handleIncrement}
-            />
-        :
-            <Loader />
-        }
+        {list.length ? (
+          <Pagination
+            page={this.state.page}
+            count={this.state.count}
+            offset={this.state.offset}
+            total={this.state.total}
+            onDecrement={this.handleDecrement}
+            onIncrement={this.handleIncrement}
+          />
+        ) : (
+          <Loader />
+        )}
       </div>
     );
   }
